a6ad03a3b869fb8cb1ba7027349576db0de98de4
OLATECH001 <olamiderashed001@gmail.com>
2025-08-07 10:51:07 +0100

    Add setup.py

--- setup.py
+++ setup.py
@@ -25,34 +25,3 @@ setup(
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
 )
-
-
-
-
-
-"""from setuptools import setup, find_packages
-from typing import List
-
-HYPHEN_E_DOT = "-e ."
-
-def get_requirements(file_path: str) -> List[str]:
-    requirements = []
-    with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
-
-    if HYPHEN_E_DOT in requirements:
-        requirements.remove(HYPHEN_E_DOT)
-
-    return requirements
-
-setup(
-    name="mlproject",
-    version="0.0.1",
-    author="Olamide Rasheed",
-    author_email="olamide@example.com",
-    packages=find_packages(where="src"),   # ✅ Look inside src/
-    package_dir={"": "src"},               # ✅ Map root to src
-    install_requires=get_requirements("requirements.txt"),
-)"""
-
